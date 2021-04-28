5aef488e534f35d7066ded537d3f96143a5f639e
liyuefei <liyuefei@gionee.com>
2021-04-28 16:01:58 +0800

    init2

--- src/emqx_plugin_recovery.erl
+++ src/emqx_plugin_recovery.erl
@@ -14,7 +14,7 @@
 %% limitations under the License.
 %%--------------------------------------------------------------------
 
--module(emqx_plugin_template).
+-module(emqx_plugin_recovery).
 
 -include_lib("emqx/include/emqx.hrl").
 
